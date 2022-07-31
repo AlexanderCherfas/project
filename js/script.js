7bfeb452dde32b8170a80b13af72ea6c7f6986d8
Alexander Cherfas <kyokushin_odessa@ukr.net>
2022-08-01 00:26:56 +0300

    add film DB

--- js/script.js
+++ js/script.js
@@ -1,23 +1,52 @@
 "use strict";
 
-console.log('arr' + ' - object');
+// console.log('arr' + ' - object');
 
-console.log(4 + +'5');
+// console.log(4 + +'5');
 
-let incr = 10,
-	decr = 10;
+// let incr = 10,
+// 	decr = 10;
 
-incr++;
-decr--;
+// incr++;
+// decr--;
 
-console.log(incr);
-console.log(decr);
+// console.log(incr);
+// console.log(decr);
 
-console.log(12 % 4);
+// console.log(12 % 4);
 
-console.log(2 +2 * 2 !== '6');
+// console.log(2 +2 * 2 !== '6');
 
-const isChecked = false,
-	isClosed = false;
+// const isChecked = false,
+// 	isClosed = false;
 
-console.log(isChecked || !isClosed);
+// console.log(isChecked || !isClosed);
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {
+
+	},
+	actors: {
+
+	},
+	genres: [
+
+	],
+	privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+		b = prompt('На сколько оцените его?', ''),
+		c = prompt('Один из последних просмотренных фильмов?', ''),
+		d = prompt('На сколько оцените его?', '');
+		
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
